a4ade3e1d77bb6732fd6ad2607ed80cb28557877
gitcrusher <aayushguptasoni@gmail.com>
2025-07-07 04:05:20 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:music_player/onboarding_screen.dart';
-import 'package:music_player/splash_screen.dart';
-import 'package:music_player/upoad_trie.dart';
-import 'search_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
