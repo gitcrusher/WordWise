4a6a25d4d2b1984bb8fdaf531b51c2ca05f7605a
gitcrusher <aayushguptasoni@gmail.com>
2025-07-08 18:20:17 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:music_player/home_page.dart';
 import 'package:music_player/onboarding_screen.dart';
+import 'package:music_player/upoad_trie.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/wordmeaning.dart
+++ lib/wordmeaning.dart
@@ -1,9 +1,10 @@
+import 'dart:convert';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'dart:ui'; // For ImageFilter.blur
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class WordMeaningPage extends StatefulWidget {
   final String word;
@@ -15,10 +16,12 @@ class WordMeaningPage extends StatefulWidget {
 
 class _WordMeaningPageState extends State<WordMeaningPage> {
   String? meaning;
+  String? audioUrl;
   String? error;
   bool isLoading = true;
   String? videoId;
   YoutubePlayerController? _youtubeController;
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   final String youTubeApiKey = 'AIzaSyAHm2QvAm8ZDNu0Y-HVylpE8Q202V8Z0JA';
   final String merriamApiKey = 'd237d493-2e02-4e54-af72-596e4d614d42';
@@ -39,7 +42,7 @@ class _WordMeaningPageState extends State<WordMeaningPage> {
       if (videoId != null) {
         _youtubeController = YoutubePlayerController(
           initialVideoId: videoId!,
-          flags: const YoutubePlayerFlags(autoPlay: false, mute: false),
+          flags: const YoutubePlayerFlags(autoPlay: false),
         );
       }
 
@@ -59,15 +62,17 @@ class _WordMeaningPageState extends State<WordMeaningPage> {
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      if (data is List &&
-          data.isNotEmpty &&
-          data[0] is Map &&
-          data[0]['shortdef'] != null) {
-        final defs = data[0]['shortdef'];
-        if (defs.isNotEmpty) {
-          meaning = defs[0]; // First definition
-        } else {
-          throw Exception('No definitions found.');
+      if (data is List && data.isNotEmpty && data[0] is Map) {
+        final entry = data[0];
+        if (entry['shortdef'] != null && entry['shortdef'].isNotEmpty) {
+          meaning = entry['shortdef'][0];
+        }
+        // Extract audio if available
+        final soundData = entry['hwi']?['prs']?[0]?['sound']?['audio'];
+        if (soundData != null) {
+          final subDir = _getSubdirectory(soundData);
+          audioUrl =
+              'https://media.merriam-webster.com/audio/prons/en/us/mp3/$subDir/$soundData.mp3';
         }
       } else {
         throw Exception('Unexpected response format.');
@@ -75,6 +80,14 @@ class _WordMeaningPageState extends State<WordMeaningPage> {
     } else {
       throw Exception('Merriam-Webster API error: ${response.statusCode}');
     }
+  }
+
+  String _getSubdirectory(String audio) {
+    if (audio.startsWith('bix')) return 'bix';
+    if (audio.startsWith('gg')) return 'gg';
+    final first = audio[0];
+    if (RegExp(r'[0-9]').hasMatch(first)) return 'number';
+    return first;
   }
 
   Future<void> fetchYouTubeVideo(String word) async {
@@ -97,58 +110,17 @@ class _WordMeaningPageState extends State<WordMeaningPage> {
   @override
   void dispose() {
     _youtubeController?.dispose();
+    _audioPlayer.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(kToolbarHeight),
-        child: Material(
-          elevation: 0,
-          color: Colors.transparent,
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Color(0xFFFF5F6D), Color(0xFF9D50BB)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.4), // Deep shadow
-                  offset: Offset(0, 6), // Downward shadow
-                  blurRadius: 12, // Soft edges
-                  spreadRadius: 1, // Shadow size
-                ),
-              ],
-            ),
-            child: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0, // Remove default AppBar shadow
-              centerTitle: true,
-              title: Text(
-                widget.word.toUpperCase() + ' !!',
-                style: GoogleFonts.pacifico(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  color: Colors.white,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 4,
-                      color: Colors.black.withOpacity(0.3),
-                      offset: Offset(2, 2),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-
+      appBar: _buildAppBar(),
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xFFFFCBA4), Color(0xFFFF5F6D), Color(0xFF9D50BB)],
@@ -156,92 +128,139 @@ class _WordMeaningPageState extends State<WordMeaningPage> {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: SizedBox.expand(
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(color: Colors.white),
-                  )
-                : error != null
-                ? Center(
-                    child: Text(
-                      error!,
-                      style: GoogleFonts.pacifico(
-                        color: Colors.white,
-                        fontSize: 18,
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(color: Colors.white),
+              )
+            : error != null
+            ? Center(
+                child: Text(
+                  error!,
+                  style: GoogleFonts.pacifico(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(24),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Meaning:',
+                        style: GoogleFonts.pacifico(
+                          fontSize: 28,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  )
-                : SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Meaning:',
-                          style: GoogleFonts.pacifico(
-                            fontSize: 28,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Text(
-                          meaning ?? '',
-                          style: GoogleFonts.pacifico(
-                            fontSize: 22,
-                            color: Colors.white70,
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        // ✅ HR Line
-                        Divider(
+                      const SizedBox(height: 20),
+                      Text(
+                        '- ${meaning ?? ''}',
+                        style: GoogleFonts.pacifico(
+                          fontSize: 22,
                           color: Colors.white70,
-                          thickness: 2,
-                          indent: 0,
-                          endIndent: 0,
                         ),
-                        const SizedBox(height: 40),
-                        Text(
-                          'Learn with a Video:',
-                          style: GoogleFonts.pacifico(
-                            fontSize: 24,
-                            color: Colors.white,
+                      ),
+
+                      const SizedBox(height: 60),
+                      if (audioUrl != null)
+                        ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white24,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          onPressed: () async {
+                            await _audioPlayer.play(UrlSource(audioUrl!));
+                          },
+                          icon: const Icon(Icons.volume_up),
+                          label: Text(
+                            'Hear Pronunciation',
+                            style: GoogleFonts.pacifico(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
                           ),
                         ),
-                        const SizedBox(height: 30),
-                        videoId != null && _youtubeController != null
-                            ? Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black.withOpacity(0.4),
-                                      blurRadius: 12,
-                                      offset: const Offset(0, 6),
-                                    ),
-                                  ],
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(20),
-                                  child: YoutubePlayer(
-                                    controller: _youtubeController!,
-                                    showVideoProgressIndicator: true,
-                                    progressColors: const ProgressBarColors(
-                                      playedColor: Colors.deepPurple,
-                                      handleColor: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            : Text(
-                                "No video found",
-                                style: GoogleFonts.pacifico(
-                                  color: Colors.white,
+                      const SizedBox(height: 30),
+                      Divider(color: Colors.white70, thickness: 2),
+                      const SizedBox(height: 30),
+                      Text(
+                        'Learn with a Video:',
+                        style: GoogleFonts.pacifico(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      videoId != null && _youtubeController != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: YoutubePlayer(
+                                controller: _youtubeController!,
+                                showVideoProgressIndicator: true,
+                                progressColors: const ProgressBarColors(
+                                  playedColor: Colors.deepPurple,
+                                  handleColor: Colors.white,
                                 ),
                               ),
-                      ],
-                    ),
+                            )
+                          : Text(
+                              "No video found",
+                              style: GoogleFonts.pacifico(color: Colors.white),
+                            ),
+                    ],
                   ),
+                ),
+              ),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(kToolbarHeight),
+      child: Material(
+        elevation: 0,
+        color: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [Color(0xFFFF5F6D), Color(0xFF9D50BB)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.4),
+                offset: const Offset(0, 6),
+                blurRadius: 12,
+                spreadRadius: 1,
+              ),
+            ],
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              widget.word.toUpperCase() + ' !!',
+              style: GoogleFonts.pacifico(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: Colors.white,
+                shadows: [
+                  Shadow(
+                    blurRadius: 4,
+                    color: Colors.black.withOpacity(0.3),
+                    offset: const Offset(2, 2),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
