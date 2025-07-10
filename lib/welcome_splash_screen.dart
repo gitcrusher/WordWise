d3b44dfb4ae7dab746d983c44272501a2487f739
gitcrusher <aayushguptasoni@gmail.com>
2025-07-11 00:13:35 +0530

    commit

---
+++ lib/welcome_splash_screen.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:music_player/home_page.dart';
+import 'package:music_player/onboarding_screen.dart';
+
+class WelcomeSplashScreen extends StatefulWidget {
+  const WelcomeSplashScreen({super.key});
+
+  @override
+  State<WelcomeSplashScreen> createState() => _WelcomeSplashScreenState();
+}
+
+class _WelcomeSplashScreenState extends State<WelcomeSplashScreen>
+    with TickerProviderStateMixin {
+  double _scale = 0.5;
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      setState(() {
+        _scale = 1.0;
+      });
+      _controller.forward();
+    });
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFFFFCBA4), Color(0xFFFF5F6D), Color(0xFF9D50BB)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: AnimatedScale(
+            scale: _scale,
+            duration: const Duration(milliseconds: 800),
+            curve: Curves.easeOutBack,
+            child: Text(
+              'Welcome to the realm of words...',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+                letterSpacing: 1.2,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
