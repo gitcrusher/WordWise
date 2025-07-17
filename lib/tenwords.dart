ae8941413dafd1878f7dfa7e31bcf71cc917737b
gitcrusher <aayushguptasoni@gmail.com>
2025-07-17 23:38:44 +0530

    commit

--- lib/tenwords.dart
+++ lib/tenwords.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
@@ -18,51 +20,56 @@ class _TenWordsPageState extends State<TenWordsPage> {
   );
   final String apiKey = Secrets.merriamApiKey;
 
-  List<Map<String, String>> _wordsWithMeanings = [];
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  List<Map<String, String>> _wordsWithData = [];
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+    );
     _fetchWordsFromTrie();
   }
 
   void _fetchWordsFromTrie() async {
     try {
-      final DataSnapshot snapshot = await _dbRef.get();
+      final snapshot = await _dbRef.get();
       if (snapshot.exists && snapshot.value != null) {
-        Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
-
+        final data = snapshot.value as Map<dynamic, dynamic>;
         List<String> allWords = [];
         _traverseTrie(data, "", allWords);
-
-        print("✅ Fetched ${allWords.length} words from trie");
-
         allWords.shuffle();
-        List<Map<String, String>> results = [];
 
+        List<Map<String, String>> results = [];
         int index = 0;
+
         while (results.length < 10 && index < allWords.length) {
           String word = allWords[index];
-          String meaning = await _fetchMeaning(word);
+          final result = await _fetchMeaningAndAudio(word);
 
-          if (!meaning.startsWith(" ")) {
-            results.add({"word": word, "meaning": meaning});
+          if (result != null) {
+            results.add(result);
           }
+
           index++;
         }
 
         setState(() {
-          _wordsWithMeanings = results;
+          _wordsWithData = results;
         });
-      } else {
-        print(" No valid data found in trie_data");
       }
     } catch (e) {
-      print("🔥 Error fetching from trie_data: $e");
+      print("❌ Error: $e");
     }
   }
 
-  Future<String> _fetchMeaning(String word) async {
+  Future<Map<String, String>?> _fetchMeaningAndAudio(String word) async {
     final url =
         'https://www.dictionaryapi.com/api/v3/references/learners/json/$word?key=$apiKey';
 
@@ -70,21 +77,33 @@ class _TenWordsPageState extends State<TenWordsPage> {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-
         if (data is List && data.isNotEmpty && data[0] is Map) {
-          final shortdef = data[0]['shortdef'];
-          if (shortdef != null && shortdef is List && shortdef.isNotEmpty) {
-            return shortdef[0];
+          final entry = data[0];
+
+          final defs = entry['shortdef'];
+          final def = defs != null && defs.isNotEmpty ? defs[0] : null;
+
+          final soundData = entry['hwi']?['prs']?[0]?['sound']?['audio'];
+          if (def != null && soundData != null) {
+            final subDir = _getSubdirectory(soundData);
+            final audioUrl =
+                'https://media.merriam-webster.com/audio/prons/en/us/mp3/$subDir/$soundData.mp3';
+
+            return {'word': word, 'meaning': def, 'audio': audioUrl};
           }
         }
-        return " Meaning not found";
-      } else {
-        return " API error";
       }
     } catch (e) {
-      print("Error fetching meaning for $word: $e");
-      return " Error loading meaning";
+      print("Error getting data for $word: $e");
     }
+    return null;
+  }
+
+  String _getSubdirectory(String soundName) {
+    if (soundName.startsWith('bix')) return 'bix';
+    if (soundName.startsWith('gg')) return 'gg';
+    final ch = soundName[0];
+    return RegExp(r'[a-zA-Z]').hasMatch(ch) ? ch : 'number';
   }
 
   void _traverseTrie(
@@ -95,17 +114,19 @@ class _TenWordsPageState extends State<TenWordsPage> {
     if (node['isWord'] == true || node['end'] == true) {
       words.add(prefix);
     }
-
     for (var entry in node.entries) {
-      if (entry.key != 'isWord' && entry.key != 'end') {
-        if (entry.value is Map) {
-          _traverseTrie(
-            entry.value as Map<dynamic, dynamic>,
-            prefix + entry.key,
-            words,
-          );
-        }
+      if (entry.key != 'isWord' && entry.key != 'end' && entry.value is Map) {
+        _traverseTrie(entry.value, prefix + entry.key, words);
       }
+    }
+  }
+
+  void _playAudio(String url) async {
+    try {
+      await _audioPlayer.stop();
+      await _audioPlayer.play(UrlSource(url));
+    } catch (e) {
+      print("Error playing audio: $e");
     }
   }
 
@@ -119,7 +140,12 @@ class _TenWordsPageState extends State<TenWordsPage> {
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context),
         ),
-        title: const Text('10 Words a Day'),
+        title: Text(
+          'Learn Something New',
+          style: GoogleFonts.pacifico(color: Colors.white),
+        ),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Colors.deepPurple.shade700,
       ),
       body: Container(
@@ -130,13 +156,15 @@ class _TenWordsPageState extends State<TenWordsPage> {
             colors: [Color(0xFF2A004F), Color(0xFF4B0082), Color(0xFF8A2BE2)],
           ),
         ),
-        child: _wordsWithMeanings.isEmpty
+        child: _wordsWithData.isEmpty
             ? const Center(child: CircularProgressIndicator())
             : PageView.builder(
                 controller: controller,
                 scrollDirection: Axis.vertical,
-                itemCount: _wordsWithMeanings.length,
+                itemCount: _wordsWithData.length,
                 itemBuilder: (context, index) {
+                  final wordData = _wordsWithData[index];
+
                   return AnimatedBuilder(
                     animation: controller,
                     builder: (context, child) {
@@ -150,54 +178,75 @@ class _TenWordsPageState extends State<TenWordsPage> {
                       );
                     },
                     child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          gradient: const LinearGradient(
-                            colors: [
-                              Color(0xFFFFCBA4), // Soft Peach
-                              Color(0xFFFF5F6D), // Warm Coral
-                              Color(0xFF9D50BB), // Purple
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              offset: const Offset(4, 8),
-                              blurRadius: 12,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 40,
+                      ),
+                      child: SizedBox(
+                        height: 450,
+                        width: double.infinity,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFFFFCBA4),
+                                Color(0xFFFF5F6D),
+                                Color(0xFF9D50BB),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
                             ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 25,
-                            vertical: 30,
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                '${index + 1}. ${_wordsWithMeanings[index]["word"]}',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.pacifico(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              const SizedBox(height: 30),
-                              Text(
-                                _wordsWithMeanings[index]["meaning"] ?? '',
-                                textAlign: TextAlign.center,
-                                style: GoogleFonts.pacifico(
-                                  fontSize: 20,
-                                  color: Colors.white.withOpacity(0.95),
-                                ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                offset: const Offset(4, 8),
+                                blurRadius: 12,
                               ),
                             ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(25),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  wordData["word"] ?? '',
+                                  textAlign: TextAlign.center,
+                                  style: GoogleFonts.pacifico(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Divider(color: Colors.white70, thickness: 2),
+                                const SizedBox(height: 20),
+                                Flexible(
+                                  child: SingleChildScrollView(
+                                    child: Text(
+                                      '- ${wordData["meaning"] ?? ''}',
+                                      textAlign: TextAlign.center,
+                                      style: GoogleFonts.pacifico(
+                                        fontSize: 20,
+                                        color: Colors.white.withOpacity(0.95),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                Divider(color: Colors.white70, thickness: 2),
+                                const SizedBox(height: 20),
+                                IconButton(
+                                  icon: const Icon(
+                                    Icons.volume_up,
+                                    size: 32,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () =>
+                                      _playAudio(wordData['audio'] ?? ''),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
