d5f2634f0547dd51e817f15142f92b5ce9aa64e4
gitcrusher <aayushguptasoni@gmail.com>
2026-02-26 15:17:28 +0530

    commit the logic

--- lib/models/trie.dart
+++ lib/models/trie.dart
@@ -20,22 +20,25 @@ class Trie {
   }
 
   List<String> startsWith(String prefix) {
+    // yahan pr hum character ke liye list bhejenege
     TrieNode? node = root;
     for (var ch in prefix.split('')) {
-      if (!node!.children.containsKey(ch)) return [];
-      node = node.children[ch];
+      if (!node!.children.containsKey(ch))
+        return []; // check karo ki char exist krta hai kya
+      node = node.children[ch]; // usi child pr move karo
     }
 
-    List<String> results = [];
+    List<String> results = []; // store all matching word
 
     void dfs(TrieNode node, String current) {
-      if (node.isEndOfWord) results.add(current);
+      if (node.isEndOfWord) results.add(current); // agar end mil gaya  add it.
       node.children.forEach((char, child) {
-        dfs(child, current + char);
+        // loop to go deeper into every child
+        dfs(child, current + char); // keep building the word.
       });
     }
 
-    dfs(node!, prefix);
+    dfs(node!, prefix); //  ye krne se dfs search for prefix + anything
     return results;
   }
 }
